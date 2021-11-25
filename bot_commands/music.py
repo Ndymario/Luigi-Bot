d7aeb672254d44759725dec59e0093b917599c65
Nolan Y? <ndymario@gmail.com>
2021-11-24 22:40:23 -0700

    Get basic music support working!

--- bot_commands/music.py
+++ bot_commands/music.py
@@ -1,15 +1,20 @@
-from discord import FFmpegPCMAudio
+from discord import FFmpegPCMAudio, VoiceClient, utils
+from discord_slash.utils.manage_commands import create_option
 import asyncio
 
-def define_slash(guild_ids, slash):
+# Define a function that will add the appropriate slash commands whent he bot starts
+def define_slash(guild_ids, slash, client):
     voice = 0
 
+    # Join the channel the user is in
     @slash.slash(name="join", description="Summon DJ Luigi Bot to the party!",guild_ids=guild_ids)
     async def join(ctx):
-        channel = ctx.author.voice.channel
-        global voice
-        voice = await channel.connect()
-        await ctx.send("Let's get this party started!", hidden=True)
+        try:
+            channel = ctx.author.voice.channel
+            voice = await channel.connect()
+            await ctx.send("Let's get this party started!", hidden=True)
+        except(AttributeError):
+            await ctx.send("You're not in a VC!", hidden=True)
 
     @slash.slash(name="leave", description="Party's over, go home DJ Luigi Bot",guild_ids=guild_ids)
     async def leave(ctx):
@@ -17,12 +22,13 @@ def define_slash(guild_ids, slash):
         await voice.disconnect()
         await ctx.send("See you next time!", hidden=True)
 
-    @slash.slash(name="play", description="Play some tunes!",guild_ids=guild_ids)
-    async def play(ctx):
-        global voice
-        player = voice.create_ffmpeg_player('Boat Battle - Bonus Time.mp3', after=lambda: print('done'))
-        player.start()
-        while not player.is_done():
-            await asyncio.sleep(1)
-        # disconnect after the player has finished
-        player.stop()
+    @slash.slash(name="play", description="Play some tunes!",guild_ids=guild_ids, options=[create_option(name="song",description="The song you wish to play!",option_type=3,required=True)])
+    async def play(ctx, song: str):
+        guild = ctx.guild
+        voice_client: VoiceClient = utils.get(client.voice_clients, guild=guild)
+        audio_source = FFmpegPCMAudio('Boat Battle - Bonus Time.mp3')
+        await ctx.send("Now playing: {0}".format(song), hidden=True)
+        if not voice_client.is_playing():
+            voice_client.play(audio_source, after=None)
+        #except(AttributeError):
+            #await ctx.send("You're not in a VC!", hidden=True)
--- main.py
+++ main.py
@@ -17,13 +17,13 @@ token = token_file.read()
 
 client = commands.Bot(command_prefix='$', intents=discord.Intents.all())
 slash = SlashCommand(client, sync_commands=True)
-guild_ids = [634581944025219091, 787615718090080286, 692071084014567534]
+guild_ids = [634581944025219091, 787615718090080286, 692071084014567534, 911507071558623302]
 
 # Set up slash commands from other files
 ch.define_slash(guild_ids, slash)
 quest.define_slash(guild_ids, slash)
 general.define_slash(guild_ids, slash)
-music.define_slash(guild_ids, slash)
+music.define_slash(guild_ids, slash, client)
 
 # Connect to our MongoDB
 mongo_db.db_connect()
@@ -35,7 +35,7 @@ async def on_ready():
 
 @client.event
 async def on_message(ctx):
-    # This try-except block is used for when a user is creating a character. 
+    # This try-except block is used for when a user is creating a character.
     # This is used to delete any messages sent for user input (characterer name & avatar link)
     try:
         # Make sure that we're deleteing the correct person's message and that we should be deleting it for the character name
@@ -43,15 +43,15 @@ async def on_message(ctx):
             ch.character_dict[ctx.author.name].char_name = ctx.content
             ch.character_dict[ctx.author.name].char_name_wait = False
             await ctx.delete()
-        
+
         # Make sure that we're deleteing the correct person's message and that we should be deleting it for the character avatar link
         if ((ctx.author.name == ch.character_dict[ctx.author.name].char_name_user) & ch.character_dict[ctx.author.name].char_avatar_wait):
             ch.character_dict[ctx.author.name].char_avatar = ctx.content
             ch.character_dict[ctx.author.name].char_avatar_wait = False
             await ctx.delete()
-    
+
     # Catch the exception if a person who is not making a character sends a message
     except(KeyError):
         return
-        
+
 client.run(token)
