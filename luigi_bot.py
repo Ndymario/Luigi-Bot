275310489c8cf20f9e48c7180a1cad0cefbdf50e
Nolan Y? <ndymario@gmail.com>
2022-03-07 19:26:35 -0600

    Begin Luigi Bot Dashboard

---
+++ luigi_bot.py
@@ -0,0 +1,87 @@
+##############
+# Luigi Bot is a bot created by Ndymario#2326!
+##############
+
+import discord
+from discord.ext import commands
+from pycord.ext import ipc
+import mongo_db.mongo_db as mongo_db
+import bot_commands.char as ch
+#import bot_commands.quest as quest
+import bot_commands.general_commands as general
+import bot_commands.music as music
+
+# Since we're good and safe coders, we will read our token from an external file
+token_file = open("token.txt", "r")
+token = token_file.read()
+token_file.close()
+
+secret_file = open("secret.txt", "r")
+secret = secret_file.read()
+secret_file.close()
+
+class LuigiBot(commands.Bot):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.ipc = ipc.Server(self,secret_key = secret)
+
+    # When the bot is ready, say so in the console
+    async def on_ready(self):
+        print("Luigi Bot Ready!")
+    
+    # When the IPC is ready, say so in the console
+    async def on_ipc_ready(self):
+        print("Ipc Server Ready!")
+    
+    # If something goes wrong with IPC, handel it
+    async def on_ipc_error(self, endpoint, error):
+        print(endpoint, "raised", error)
+
+bot = LuigiBot(command_prefix='$', intents = discord.Intents.default())
+guild_ids = [334848602130219009, 427651547208482816, 787615718090080286, 399424476259024897, 634581944025219091, 915655039463333899, 813610703381987408]
+
+# Set up slash commands from other files
+ch.define_slash(guild_ids, bot)
+general.define_slash(guild_ids, bot)
+
+# Until the music re-write happens, the music commands are offline!
+# music.define_slash(guild_ids, bot)
+
+# Likewise, until the RPG engine is implemented, the quest commands are offline!
+# quest.define_slash(guild_ids, bot)
+
+# Connect to our MongoDB
+mongo_db.db_connect()
+
+@bot.ipc.route()
+async def get_guild_count(data):
+	return len(bot.guilds) # returns the len of the guilds to the client
+
+@bot.ipc.route()
+async def get_guild_ids(data):
+	final = []
+	for guild in bot.guilds:
+		final.append(guild.id)
+	return final # returns the guild ids to the client
+
+@bot.ipc.route()
+async def get_guild(data):
+	guild = bot.get_guild(data.guild_id)
+	if guild is None: return None
+
+	guild_data = {
+		"name": guild.name,
+		"id": guild.id,
+		"prefix" : "?"
+	}
+
+	return guild_data
+
+# When the bot is ready, say so in the console
+@bot.event
+async def on_ready():
+    print("Ready!")
+
+# Run the bot!
+bot.ipc.start()
+bot.run(token)
--- main.py
+++ main.py
@@ -1,38 +1,81 @@
-##############
-# Luigi Bot is a bot created by Ndymario#2326!
-##############
+from quart import Quart, render_template, request, session, redirect, url_for
+from quart_discord import DiscordOAuth2Session
+from pycord.ext import ipc
 
-from discord.ext import commands
-import mongo_db.mongo_db as mongo_db
-import bot_commands.char as ch
-import bot_commands.quest as quest
-import bot_commands.general_commands as general
-import bot_commands.music as music
+secret_file = open("secret_dash.txt", "r")
+secret = secret_file.read()
+secret_file.close()
 
-# Since we're good and safe coders, we will read our token from an external file
-token_file = open("token.txt", "r")
-token = token_file.read()
+client_id_file = open("client_id.txt", "r")
+client_id = client_id_file.read()
+client_id_file.close()
 
-bot = commands.Bot(command_prefix='$')
-guild_ids = [334848602130219009, 427651547208482816, 787615718090080286, 399424476259024897, 634581944025219091, 915655039463333899, 813610703381987408]
+client_secret_file = open("client_secret.txt", "r")
+client_secret = client_secret_file.read()
+client_secret_file.close()
 
-# Set up slash commands from other files
-ch.define_slash(guild_ids, bot)
-general.define_slash(guild_ids, bot)
+redirect_uri_file = open("redirect_uri.txt", "r")
+redirect_uri = redirect_uri_file.read()
+redirect_uri_file.close()
 
-# Until the music re-write happens, the music commands are offline!
-# music.define_slash(guild_ids, bot)
+app = Quart(__name__)
+ipc_client = ipc.Client(secret_key = secret)
 
-# Likewise, until the RPG engine is implemented, the quest commands are offline!
-# quest.define_slash(guild_ids, bot)
+app.config["SECRET_KEY"] = secret
+app.config["DISCORD_CLIENT_ID"] = client_id
+app.config["DISCORD_CLIENT_SECRET"] = client_secret
+app.config["DISCORD_REDIRECT_URI"] = redirect_uri 
 
-# Connect to our MongoDB
-mongo_db.db_connect()
+discord = DiscordOAuth2Session(app)
 
-# When the bot is ready, say so in the console
-@bot.event
-async def on_ready():
-    print("Ready!")
+@app.route("/")
+async def home():
+	return await render_template("index.html", authorized = await discord.authorized)
 
-# Run the bot!
-bot.run(token)
+@app.route("/login")
+async def login():
+	return await discord.create_session()
+
+@app.route("/callback")
+async def callback():
+	try:
+		await discord.callback()
+	except Exception:
+		pass
+
+	return redirect(url_for("dashboard"))
+
+@app.route("/dashboard")
+async def dashboard():
+	if not await discord.authorized:
+		return redirect(url_for("login")) 
+
+	guild_count = await ipc_client.request("get_guild_count")
+	guild_ids = await ipc_client.request("get_guild_ids")
+
+	user_guilds = await discord.fetch_guilds()
+
+	guilds = []
+
+	for guild in user_guilds:
+		if guild.permissions.administrator:			
+			guild.class_color = "green-border" if guild.id in guild_ids else "red-border"
+			guilds.append(guild)
+
+	guilds.sort(key = lambda x: x.class_color == "red-border")
+	name = (await discord.fetch_user()).name
+	return await render_template("dashboard.html", guild_count = guild_count, guilds = guilds, username=name)
+
+@app.route("/dashboard/<int:guild_id>")
+async def dashboard_server(guild_id):
+	if not await discord.authorized:
+		return redirect(url_for("login")) 
+
+	guild = await ipc_client.request("get_guild", guild_id = guild_id)
+	if guild is None:
+		return redirect(f'https://discord.com/oauth2/authorize?&client_id={app.config["DISCORD_CLIENT_ID"]}&scope=bot&permissions=8&guild_id={guild_id}&response_type=code&redirect_uri={app.config["DISCORD_REDIRECT_URI"]}')
+	return guild["name"]
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
